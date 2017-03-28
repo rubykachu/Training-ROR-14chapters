f7a5bc2f37c55d76349b9873b15f625e66c5cc61
Minh Tang Q <minh.tang@asiantech.vn>
2017-03-28 11:56:29 +0700

    Lear synatra

---
+++ sinatra/config.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+Tilt.register Tilt::ERBTemplate, 'html.erb'
+class MyApp < Sinatra::Base
+	LOG_FILE = "log.txt"
+	def self.run!
+		super
+		if !File.exists?(LOG_FILE)
+			File.open(LOG_FILE, 'w') do |file|
+				file.puts "Time/Title/Content"
+			end
+		end
+	end
+
+	get '/' do
+		erb:index
+	end
+
+	post '/' do
+		@title   = params[:title]
+		@content = params[:content]
+		File.open(LOG_FILE, 'a') do |file|
+			time    = Time.now
+			file.puts "#{time}/#{@title}/#{@content}"
+			erb:index
+		end
+	end
+end
+
+MyApp.run!
