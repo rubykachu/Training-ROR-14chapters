243a5af52ba53d9a6e008fd70f02566d3c3a8f5a
Minh Tang Q <minh.tang@asiantech.vn>
2017-03-31 15:31:09 +0700

    Shopping cart with Jquery

---
+++ HTML-TRAVEL/js/modules/home/concerns/data.js
@@ -0,0 +1,50 @@
+var data = {
+	"article-1.png" : {
+		"title" : "hà nội - phú quốc",
+		"price" : "5.350.000"
+	},
+	"article-2.png" : {
+		"title" : "huế - đà nẵng",
+		"price" : "4.370.000"
+	},
+	"article-3.png" : {
+		"title" : "hải phòng - nha trang",
+		"price" : "6.550.000"
+	},
+	"article-4.png" : {
+		"title" : "đà nẵng - lạng sơn",
+		"price" : "8.370.000"
+	},
+	"article-5.png" : {
+		"title" : "hồ chí minh - đà nẵng",
+		"price" : "7.750.000"
+	},
+	"article-6.png" : {
+		"title" : "huế - đà nẵng",
+		"price" : "4.370.000"
+	},
+	"article-7.png" : {
+		"title" : "hà nội - phú quốc",
+		"price" : "5.350.000"
+	},
+	"article-8.png" : {
+		"title" : "huế - đà nẵng",
+		"price" : "4.370.000"
+	},
+	"article-9.png" : {
+		"title" : "hà nội - phú quốc",
+		"price" : "5.350.000"
+	},
+	"article-10.png" : {
+		"title" : "huế - đà nẵng",
+		"price" : "4.370.000"
+	},
+	"article-11.png" : {
+		"title" : "hà nội - phú quốc",
+		"price" : "5.350.000"
+	},
+	"article-12.png" : {
+		"title" : "huế - đà nẵng",
+		"price" : "4.370.000"
+	}
+}
