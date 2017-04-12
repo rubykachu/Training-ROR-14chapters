05a9cdcf38649b7689e20508708efd9fc3baa5ca
Minh Tang Q <minh.tang@asiantech.vn>
2017-04-12 14:15:05 +0700

    update routes

--- routes.rb
+++ routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/index' => 'persons#list'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :index, :create, :update, :delete] do
+  resources :users, only: [:show, :index, :create, :update, :destroy] do
   	member do
   		get :posts, :recent_posts
   		post :posts
